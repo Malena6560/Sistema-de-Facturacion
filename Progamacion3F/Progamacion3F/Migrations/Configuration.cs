109efb213241dc0f17c3147929def5249af2b119
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-06 18:47:15 -0400

    Sistema de Fcaturacion
    Creacion de la Clase MantenimientosContext, el enlace con labase de datos mediante Code First

---
+++ Progamacion3F/Progamacion3F/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Progamacion3F.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Progamacion3F.Models.MatenimientosContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Progamacion3F.Models.MatenimientosContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Progamacion3F/Progamacion3F/Models/MatenimientosContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Progamacion3F.Models
+{
+    public class MatenimientosContext:DbContext
+    {
+        
+        public DbSet<Productos> Producto { get; set; }
+        public DbSet<Clientes> Cliente { get; set; }
+        public DbSet<Proveedores> Proveedor { get; set; }
+    }
+}

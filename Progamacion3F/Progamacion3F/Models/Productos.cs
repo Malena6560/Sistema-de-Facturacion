e886d3195f27ddad37dc8385fb8e741b7b3cadb9
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-06 17:58:38 -0400

    Sistema de Facturacion
    Creacion de la clase Productos

---
+++ Progamacion3F/Progamacion3F/Models/Productos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Progamacion3F.Models
+{
+    public class Productos
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}

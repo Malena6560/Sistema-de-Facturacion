fb333a37d2eb26078f8799a4b164c728cc8f72c3
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-06 18:01:44 -0400

    Sistema de Facturacion
    Creacion de la Clase Clientes

---
+++ Progamacion3F/Progamacion3F/Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Progamacion3F.Models
+{
+    public class Clientes
+    {
+        public int ID { get; set; }
+        public int RNC { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public string Categoria { get; set; }
+    }
+}

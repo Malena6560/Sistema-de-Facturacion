236439de46c550b8c65e3b0d8ab42fb5373a41cb
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-06 18:06:10 -0400

    Sistema de Facturacion
    Creacion de la Clase Proveedores

---
+++ Progamacion3F/Progamacion3F/Models/Proveedores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Progamacion3F.Models
+{
+    public class Proveedores
+    {
+        public int ID { get; set; }
+        public int RNC { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
